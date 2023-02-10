20f9bd3804a1cedce57dc319b79ec4668d3c5d33
Ryan <kenidyryan@gmail.com>
2023-02-10 11:35:47 +0700

    change main colour and not found page

--- src/not-found-page.js
+++ src/not-found-page.js
@@ -1,16 +1,4 @@
-import { useRouteError } from "react-router-dom";
-import { Flex, Container, Paragraph, Header } from "./components/styles/UI";
+// import { useRouteError } from "react-router-dom";
 export default function ErrorPage() {
-  const error = useRouteError();
-  console.error(error);
-
-  return (
-    <Container id="error-page" height="50%" width="50%" center={true}>
-      <Header margin="0 0 2rem">Oops!</Header>
-      <Paragraph margin="0 0 1rem">
-        Sorry, an unexpected error has occurred
-      </Paragraph>
-      <Paragraph>{error.statusText || error.message}</Paragraph>
-    </Container>
-  );
+    return <div>Not Found</div>;
 }
