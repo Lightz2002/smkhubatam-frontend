1281f72968827d1d21663e4b40c0c3d632a732a9
Ryan <kenidyryan@gmail.com>
2023-02-10 11:35:19 +0700

    submit login, and format data

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -10,120 +10,120 @@ import { initialQuery } from "../../api/queries";
 import { useTheme } from "@emotion/react";
 
 export const action =
-  (queryClient) =>
-  async ({ request, params }) => {
-    try {
-      const formData = await request.formData();
-      const credentials = Object.fromEntries(formData);
-      queryClient.invalidateQueries(["user"]);
-      const res = await login(credentials);
-      if (res.statusCode === 401) return;
+    (queryClient) =>
+    async ({ request, params }) => {
+        try {
+            const formData = await request.formData();
+            const credentials = Object.fromEntries(formData);
+            queryClient.invalidateQueries(["user"]);
+            const res = await login(credentials);
+            if (res.statusCode === 401) return;
 
-      // if credentials are correct
-      setToken(res.data.access_token);
-      localStorage.setItem("token", token);
-      return redirect("/");
-    } catch (e) {
-      console.warn(e);
-    }
-  };
+            // if credentials are correct
+            setToken(res.data.access_token);
+            localStorage.setItem("token", token);
+            return redirect("/");
+        } catch (e) {
+            console.warn(e);
+        }
+    };
 
 export const loader =
-  (queryClient) =>
-  async ({ request, params }) => {
-    try {
-      const query = initialQuery();
-      return (
-        queryClient.getQueryData(query) ?? (await queryClient.fetchQuery(query))
-      );
-    } catch (e) {
-      return e;
-    }
-  };
+    (queryClient) =>
+    async ({ request, params }) => {
+        try {
+            const query = initialQuery();
+            return queryClient.getQueryData(query) ?? (await queryClient.fetchQuery(query));
+        } catch (e) {
+            return e;
+        }
+    };
 
 const Login = () => {
-  const theme = useTheme();
-  const [user, setUser] = useState({
-    Username: "",
-    Password: "",
-  });
-
-  const handleUsernameChange = (e) => {
-    setUser((user) => {
-      return {
-        ...user,
-        Username: e.target.value,
-      };
+    const theme = useTheme();
+    const [user, setUser] = useState({
+        Username: "",
+        Password: "",
     });
-  };
-  const handlePasswordChange = (e) => {
-    setUser((user) => {
-      return {
-        ...user,
-        Password: e.target.value,
-      };
-    });
-  };
 
-  const handleSubmit = (input) => {
-    setUser({
-      Username: "",
-      Password: "",
-    });
-  };
+    const handleUsernameChange = (e) => {
+        setUser((user) => {
+            return {
+                ...user,
+                Username: e.target.value,
+            };
+        });
+    };
+    const handlePasswordChange = (e) => {
+        setUser((user) => {
+            return {
+                ...user,
+                Password: e.target.value,
+            };
+        });
+    };
 
-  return (
-    <Box
-      sx={{
-        position: "relative",
-        transform: "translateY(-50%)",
-        top: "50%",
-        height: "70%",
-        width: "60%",
-        boxShadow: 1,
-        padding: 0,
-        margin: "0 auto",
-      }}
-    >
-      <Grid
-        container
-        sx={{
-          height: "100%",
-          width: "100%",
-        }}
-      >
-        <Grid
-          item
-          xs={3}
-          alignItems="center"
-          sx={{
-            background: theme.palette.primary.main,
-            height: "100%",
-          }}
+    const handleSubmit = (input) => {
+        setUser({
+            Username: "",
+            Password: "",
+        });
+    };
+
+    return (
+        <Box
+            sx={{
+                position: "relative",
+                transform: "translateY(-50%)",
+                top: "50%",
+                height: "70%",
+                width: "60%",
+                boxShadow: 1,
+                padding: 0,
+                margin: "0 auto",
+            }}
         >
-          <Typography>SMK HU BATAM</Typography>
-        </Grid>
-        <Grid
-          item
-          xs={6}
-          sx={{
-            padding: "2rem",
-          }}
-        >
-          <Typography variant="h5" sx={{ mb: 5 }}>
-            Login
-          </Typography>
-          <LoginForm
-            credentials={user}
-            handleUsernameChange={handleUsernameChange}
-            handlePasswordChange={handlePasswordChange}
-            handleSubmit={handleSubmit}
-            action="/login"
-          />
-        </Grid>
-      </Grid>
-    </Box>
-  );
+            <Grid
+                container
+                sx={{
+                    height: "100%",
+                    width: "100%",
+                }}
+            >
+                <Grid
+                    item
+                    xs={3}
+                    alignItems='center'
+                    sx={{
+                        background: theme.palette.primary.main,
+                        height: "100%",
+                    }}
+                >
+                    <Typography sx={{ color: "white", textAlign: "center" }}>
+                        SMK HU BATAM
+                    </Typography>
+                </Grid>
+                <Grid
+                    item
+                    xs={6}
+                    sx={{
+                        padding: "2rem",
+                    }}
+                >
+                    <Typography variant='h5' sx={{ mb: 5 }}>
+                        Login
+                    </Typography>
+                    <LoginForm
+                        credentials={user}
+                        handleUsernameChange={handleUsernameChange}
+                        handlePasswordChange={handlePasswordChange}
+                        handleSubmit={handleSubmit}
+                        action='/login'
+                    />
+                </Grid>
+            </Grid>
+        </Box>
+    );
 };
 
 export default Login;
--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -4,45 +4,47 @@ import { useQueryClient, useQuery, useMutation } from "react-query";
 import { Button, TextField } from "@mui/material";
 
 const LoginForm = ({
-  credentials,
-  handlePasswordChange,
-  handleUsernameChange,
-  method = "post",
-  action,
-  handleSubmit,
+    credentials,
+    handlePasswordChange,
+    handleUsernameChange,
+    method = "post",
+    action,
+    handleSubmit,
 }) => {
-  return (
-    <Form method={method} action={action} onSubmit={handleSubmit}>
-      <TextField
-        id="Username"
-        name="Username"
-        label="Username"
-        variant="outlined"
-        value={credentials.Username}
-        onChange={handleUsernameChange}
-        margin="normal"
-      />
-      <TextField
-        id="Password"
-        name="Password"
-        label="Password"
-        type="password"
-        variant="outlined"
-        value={credentials.Password}
-        onChange={handlePasswordChange}
-        margin="normal"
-      />
+    return (
+        <Form method={method} action={action} onSubmit={handleSubmit}>
+            <TextField
+                id='Username'
+                name='Username'
+                label='Username'
+                variant='outlined'
+                value={credentials.Username}
+                onChange={handleUsernameChange}
+                margin='normal'
+            />
+            <TextField
+                id='Password'
+                name='Password'
+                label='Password'
+                type='password'
+                variant='outlined'
+                value={credentials.Password}
+                onChange={handlePasswordChange}
+                margin='normal'
+            />
 
-      <Button
-        type="submit"
-        sx={{
-          display: "block",
-        }}
-      >
-        Login
-      </Button>
-    </Form>
-  );
+            <Button
+                type='submit'
+                variant='contained'
+                sx={{
+                    display: "block",
+                    mt: 4,
+                }}
+            >
+                Login
+            </Button>
+        </Form>
+    );
 };
 
 export default LoginForm;
