8486011a83221f1c2e169b1cdf117de41cd51431
Ryan kenidy <kenidyryan@gmail.com>
2023-02-19 07:29:51 +0700

    change app palette and tabs

--- src/App.js
+++ src/App.js
@@ -6,29 +6,33 @@ import router from "./routes/router.js";
 import { blue } from "@mui/material/colors";
 
 function App() {
-    const queryClient = new QueryClient();
-    const theme = createTheme({
-        palette: {
-            primary: {
-                main: "#439A97",
-            },
-        },
-    });
+  const queryClient = new QueryClient();
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#439A97",
+      },
+      warning: {
+        main: "#d32f2f",
+        dark: "#c62828",
+      },
+    },
+  });
 
-    return (
-        <ThemeProvider theme={theme}>
-            <QueryClientProvider client={queryClient}>
-                <Box
-                    className='App'
-                    sx={{
-                        height: "100%",
-                    }}
-                >
-                    <RouterProvider router={router} />
-                </Box>
-            </QueryClientProvider>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <Box
+          className="App"
+          sx={{
+            height: "100%",
+          }}
+        >
+          <RouterProvider router={router} />
+        </Box>
+      </QueryClientProvider>
+    </ThemeProvider>
+  );
 }
 
 export default App;
